c2a077fb3297b8ebee6643fc6f39bcface4bdea9
Osman Beltran <osmancadc@hotmail.com>
2022-08-15 23:29:01 -0500

    test V7

--- UploadScore/main.go
+++ UploadScore/main.go
@@ -10,7 +10,7 @@ import (
 )
 
 /*
-* Little change to check the linter and deploy functionality V7
+* Little change to check the linter and deploy functionality V8
  */
 func HanderUploadScore(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	var reqBody RequestBody

aefaf9390d13823649811643607160f25b9f42bb
Osman Beltran <osmancadc@hotmail.com>
2022-08-31 21:53:04 -0500

    Authentication and Creation endpoints added

--- AuthenticateUser/main_test.go
+++ AuthenticateUser/main_test.go
@@ -21,7 +21,7 @@ func TestHanderUploadScore(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := HanderUploadScore(tt.args.req)
+			got, err := HanderAuthenticateUser(tt.args.req)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("HanderUploadScore() error = %v, wantErr %v", err, tt.wantErr)
 				return

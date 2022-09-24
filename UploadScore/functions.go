5d3b1b1371b64f006db273501e6fc878cd4b786d
Osman Beltran <osmancadc@hotmail.com>
2022-09-24 08:00:57 -0500

    updated imports

--- UploadScore/functions.go
+++ UploadScore/functions.go
@@ -4,9 +4,17 @@ import (
 	"database/sql"
 	"fmt"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func ConnectDatabase() (connection *sql.DB) {
+
+	os.Setenv("DB_USER", "administrator")
+	os.Setenv("DB_PASSWORD", "35Yw!8uO5v5g")
+	os.Setenv("DB_HOST", "dev-asegurate.cluster-cnaioe8hvyno.us-east-1.rds.amazonaws.com")
+	os.Setenv("DB_NAME", "dev_asegurate")
+
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
 	host := os.Getenv("DB_HOST")
@@ -14,13 +22,12 @@ func ConnectDatabase() (connection *sql.DB) {
 
 	connection, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s", user, password, host, database))
 	if err != nil {
-		fmt.Printf(`Error conectando DB %s`, err.Error())
+		fmt.Printf("ConnectDatabase(1) %s", err.Error())
 		panic(err.Error())
 	}
 
 	return
 }
-
 func UploadScore(conn *sql.DB, author, stars int, objective, comments string) error {
 	query, err := conn.Prepare(`INSERT INTO dev_asegurate.score (author, objective, stars, coments) VALUES(?, ?, ?, ?); `)
 	if err != nil {
